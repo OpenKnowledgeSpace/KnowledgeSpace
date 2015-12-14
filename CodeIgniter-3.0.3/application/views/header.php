b3d9cc41aec923e0d951c7b4568818eee174dcd7
Willy Wong <wawong@gmail.com>
2015-12-14 15:25:50 -0800

    commit

---
+++ CodeIgniter-3.0.3/application/views/header.php
@@ -0,0 +1,10 @@
+<html>
+        <head>
+                <title>CodeIgniter Tutorial</title>
+        </head>
+        <body>
+
+                <h1><?php echo $title; ?></h1>
+		<em>&copy; 2015</em>
+        </body>
+</html>
