13c2d261e32ee5c96f09b396e7eb0874c70d0257
System Administrator <root@ncmirs-mbp.dynamic.ucsd.edu>
2016-05-23 16:44:19 -0700

    Add overview_url in API

--- NeuroKS/application/views/api/view_summary.php
+++ NeuroKS/application/views/api/view_summary.php
@@ -1,12 +1,13 @@
 <?php
 header('Content-Type: application/json');
-
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$domainName = $_SERVER['HTTP_HOST'];
 ////var_dump($config_array);
 //var_dump($category_count_array);
 $categories =  $config_array->categories;
 
 $root = array();
-
+$root["overview_url"] = $protocol.$domainName."/".$config_array->local_context_name."/index.php/pages/view/".$curie;
 foreach($categories as $category)
 {
     //$root[$category] = array();
@@ -25,10 +26,13 @@ foreach($categories as $category)
         $root[$category][$source->curie]['name'] = $source->source_name;
         $root[$category][$source->curie]['count'] = $result->result->resultCount;
         $root[$category][$source->curie]['description'] = $source->description;
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $domainName = $_SERVER['HTTP_HOST'];
+
         
-        $root[$category][$source->curie]['link'] = $protocol.$domainName."/".$config_array->local_context_name."/pages/view/".$curie;
+        //$root[$category][$source->curie]['link'] = $protocol.$domainName."/".$config_array->local_context_name."/pages/view/".$curie;
+        
+        $root[$category][$source->curie]['link'] = 
+                $protocol.$domainName."/".$config_array->local_context_name.
+                "/index.php?/Results/view/".$source->curie."/".$pageName."/1";
         
         /*echo "<br/>Name".$source->source_name;
         $result = $source_search_result[$source->curie];
