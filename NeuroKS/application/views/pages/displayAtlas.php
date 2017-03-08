f79ef53e1e4f341b97d8da272f39475b2aca9e1d
Michaël Dupont <mdupont@users.noreply.github.com>
2017-03-08 11:38:28 +0100

    Update Atlas Viewer URL to show Allen v3 2016 instead of Allen v3 2015

--- NeuroKS/application/views/pages/displayAtlas.php
+++ NeuroKS/application/views/pages/displayAtlas.php
@@ -1,3 +1,3 @@
 <div class="content">
-<iframe width="100%" height="800" src="https://nip.humanbrainproject.eu/atlas/#/?dataset=5954630a-ec21-11e4-a5f8-6003088da632&locked=1&overlay=1"></iframe>
+<iframe width="100%" height="800" src="https://nip.humanbrainproject.eu/atlas/#/?dataset=5954630a-ec21-11e4-a5f8-6003088da398&locked=1&overlay=1"></iframe>
 </div>
