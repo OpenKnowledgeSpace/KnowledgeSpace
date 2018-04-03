044becc630acb025e53bed8a3fb6f21a68759398
Cloud User <chrisfitzpat@gmail.com>
2018-04-03 13:49:55 +0000

    Retry request if NIF gives back a 503
    NIF Services sometimes gives back a 503. This allows us to wait then resubmit the request.

--- app/Kspace/SolrDataSpaceClient.php
+++ app/Kspace/SolrDataSpaceClient.php
@@ -24,32 +24,76 @@
  
       }
 
-      
+
+    public function searchSource( $source, $params, $try = 0)
+      {
+        try {
+          $res = $this->client->request("GET", '/api/1/dataservices/federation/data/'.$source,
+          ['query' => $params]);
+          return $res->getBody(); 
+        } catch ( \GuzzleHttp\Exception\BadResponseException $e ) {
+          if ( $try < 20 ) {
+	    sleep(2); 
+	    $this->searchSource($source, $params, $try + 1); 
+          } else {
+            return false; 
+          }
+        } 
+      }  
+
+      /* DRY this out */ 
+    public function getFacets( $source, $params, $try = 0)
+      {
+        try {
+          $facetsRes = $this->client->request("GET",
+            '/api/1/dataservices/federation/facets/'.$source, ['query' => $params]);
+          return $facetsRes->getBody(); 
+        } catch ( \GuzzleHttp\Exception\BadResponseException $e ) {
+          if ( $try < 10 ) {
+            sleep(2); 
+	    $this->searchSource($source, $params, $try + 1); 
+          } else {
+            return false; 
+          }
+        } 
+      }  
+
+      /* Swap this out to do concurrent requests... eventually
+       * However, currently I'm not sure if the NIF services server can take
+       * that kind of load. Eventually refactor this, especially the weak
+       * searchSource and getFacets methods. 
+       * */ 
     public function search( $sources, $query,  $params = array( 'count' => 5, 'offset' => 0 ))
       {
         $params = array_merge( $this->defaultParams(), $params );
         $params["q"] = $query;
         $output = []; 
         foreach ( $sources as $source  ) {
-          $res = $this->client->request("GET", '/api/1/dataservices/federation/data/'.$source, ['query' => $params]);
-          $facetsRes = $this->client->request("GET", '/api/1/dataservices/federation/facets/'.$source, ['query' => $params]);
-          
+
+          $resBody = $this->searchSource($source, $params); 
+          $facetBody = $this->getFacets($source, $params); 
+
+          if ( $resBody != false ) {
+            $output[$source]["data"] = simplexml_load_string( $resBody );
+          } 
+
           // YUCK!
           $facets = []; 
-          $xml = simplexml_load_string( $facetsRes->getBody() ); 
-          foreach ( $xml->facets as $category ) {
-            $name = (string) $category["category"];
-            $facets[$name] = []; 
-            foreach ( $category->facets as $facet ) {
-              $facetOutput = array();
-              $facetOutput["value"] = (string) $facet;
-              $facetOutput["count"] = (string) $facet["count"];  
-              array_push($facets[$name], $facetOutput); 
-            } 
-          }; 
-           
-          $output[$source]["data"] = simplexml_load_string( $res->getBody() );
-          $output[$source]["facets"] = $facets; 
+          if ( $facetBody != false && $resBody != false ) {   
+            $xml = simplexml_load_string( $facetBody ); 
+            foreach ( $xml->facets as $category ) {
+              $name = (string) $category["category"];
+              $facets[$name] = []; 
+              foreach ( $category->facets as $facet ) {
+                $facetOutput = array();
+                $facetOutput["value"] = (string) $facet;
+                $facetOutput["count"] = (string) $facet["count"];  
+                array_push($facets[$name], $facetOutput); 
+              } 
+            }; 
+            $output[$source]["facets"] = $facets; 
+          } 
+        
         }
         return json_encode($output);
       }
