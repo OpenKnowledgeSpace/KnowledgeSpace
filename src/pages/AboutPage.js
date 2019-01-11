b516e836ae24ad2dce4ae37ec4f059ad983f5298
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2019-01-11 10:10:26 +0100

    Bump about font size

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -65,9 +65,15 @@ class AboutPage extends Component {
       <Grid container direction="column" justify="flex-start" alignItems="center">
         <Grid item xs={6}>
           <Typography variant="h2" gutterBottom>About Us</Typography>
-          <Typography variant="body2" align='justify' gutterBottom>
+          <Typography variant="body1" align='justify' gutterBottom>
             KnowledgeSpace (KS) is a community encyclopaedia that links brain research concepts with data, models and literature from around the world. It is an open project and welcomes participation and contributions from members of the global research community. KS is the result of recommendations from a community workshop held by the INCF Program on Ontologies of Neural Structures in 2012 and include the report attached below to community workshop. 2012 INCF Workshop Report 
+          </Typography>
+          <br/>
+          <Typography variant="body1" align='justify' gutterBottom>
             KnowledgeSpace is currently being developed with support from the HBP Neuroinformatics Platform funded from the European Union’s Horizon 2020 Framework Programme for Research and Innovation under the Specific Grant Agreement No. 785907 (Human Brain Project SGA2), INCF, and Neuroscience Information Frameworkj (NIF). Earlier development was supported by the HBP Neuroinformatics Platform funded from the European Union’s Horizon 2020 Framework Programme for Research and Innovation under the Specific Grant Agreement No. 720270 and No. 604102 (Human Brain Project SGA1), INCF, NIF, and the Blue Brain Project.	
+          </Typography>
+          <br/>
+          <Typography variant="body1" align='justify' gutterBottom>
             KS builds on a vocabulary service, populated with an integrated set of neuroscience ontologies with initial content coming from the Neuroscience Lexicon (NeuroLex), and the Brain Architecture Management System (BAMS). It links to an expanding set of data sources through the NIF federated search infrastructure. 
           </Typography>
         </Grid>
